ed79ceaef081da1f87ffe9201fbe495f7528a4a9
dragon-web <1582943265@qq.com>
2019-12-04 18:39:09 +0800

    。。。

---
+++ myBinTree/myBinTree/Cplusplus.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Test
+{
+};
+
+void Swap(int &a, int &b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//int a = 8;
+	//int b = 6;
+	//Swap(a, b);
+	////printf("%d %d", a, b);
+	//cout <<a<<"   "<<b<< endl;
+	system("pause");
+	return 0;
+}
