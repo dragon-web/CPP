9b8db0cbc946426403c6feda05c6d4ce1d4a3556
dragon-web <1582943265@qq.com>
2020-02-18 08:40:24 +0800

    String类

---
+++ C++_2_15_String/C++_2_15_String/string.cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+﻿int  main(int argc, char *argv[])
+
+{
+	string a = "hello world";
+
+	string b = a;
+
+	if (a.c_str() == b.c_str())
+
+	{
+
+		cout << "true" << endl;
+
+	}
+
+	else cout << "false" << endl;
+
+	string c = b;
+
+	c = "";
+
+	if (a.c_str() == b.c_str())
+
+	{
+
+		cout << "true" << endl;
+
+	}
+
+	else cout << "false" << endl;
+
+	a = "";
+
+	if (a.c_str() == b.c_str())
+
+	{
+
+		cout << "true" << endl;
+
+	}
+
+	else cout << "false" << endl;
+	system("pause");
+	return 0;
+
+}
+
